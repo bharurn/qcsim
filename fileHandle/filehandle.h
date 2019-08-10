3b32e741327c840261a43067252e47b337b38d85
Bharath Raghavan <52472738+bharurn@users.noreply.github.com>
2019-08-10 03:46:33 -0400

    Create filehandle.h

---
+++ fileHandle/filehandle.h
@@ -0,0 +1,31 @@
+#ifndef FILEHANDLE_H
+#define FILEHANDLE_H
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+namespace fileHandle
+{
+	const char endl = '\n';
+	
+	class outHandle
+	{
+		public:
+			void open(std::string fname);
+			outHandle();
+			outHandle(std::string fname);
+			~outHandle();
+			
+			outHandle& operator<<(  const std::string mesg );
+			outHandle& operator<<(  const char mesg );
+			outHandle& operator<<(const int mesg );
+			outHandle& operator<<(  const double mesg );
+		
+		private:
+			std::ofstream f;		
+	};
+}
+
+
+#endif
